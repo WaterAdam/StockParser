b9d59c0eee2ead0e9d7c4ac753e5e8b9a611e06f
RihSian Chen <rihsianchen@adam.local>
2022-01-13 16:15:37 +0800

    license with setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='StockParser',
+    version='V1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='WaterAdam',
+    author_email='kent3662003@gmail.com',
+    description='V1.0'
+)
